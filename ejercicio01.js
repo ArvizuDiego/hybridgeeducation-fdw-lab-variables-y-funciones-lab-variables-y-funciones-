f006934c8ea46a54b3d119d65a9a7af5266cc662
ArvizuDiego <diego2409009@hybridge.education>
2025-12-15 11:08:51 -0600

    Update ejercicio01.js

--- ejercicio01.js
+++ ejercicio01.js
@@ -1,8 +1,10 @@
-// Ejercicio 1: Declaración de Función
+es// Ejercicio 1: Declaración de Función
 // Modifica la siguiente función creando una nueva variable llamada nombre y asignándole tu nombre. La función debe retornar esta nueva variable que contiene tu nombre
 
 function obtenerNombre() {
-    // Escribe tu código aquí
+    const nombre = "Diego Alberto Arvizu Villarreal";
+    se
+    return nombre;
 }
 
 // No modificar.
